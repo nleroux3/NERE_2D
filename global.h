9e55a5c6ba9365c4bc52689aa63244fb05096d29
Nicolas Leroux <nicolas.r.leroux@gmail.com>
2019-05-09 11:10:36 -0600

    Addition of global variables

--- global.h
+++ global.h
@@ -2,6 +2,9 @@
 // Created by Nicolas Leroux on 2019-04-16.
 //
 #pragma once
+#include <Eigen/Sparse>
+#include <Eigen/SparseLU>
+using namespace Eigen;
 
 const int Nx = 100,  // Number of cells in lateral direction
           Ny = 100;  // Number of cells in vertical direction
@@ -14,4 +17,9 @@ extern double  alpha[Ny][Nx],
                psi[2][Ny][Nx],
                K[2][Ny][Nx],
                alphaW[Ny][Nx],
-               Swf[2][Ny][Nx];
+               Swf[2][Ny][Nx],
+               tau[Ny][Nx],
+               qIn[Nx],
+               Ts[Nx],
+               T[Ny][Nx],
+               Keff[Ny][Nx]; // thermal conductivity [W/(m K)];
